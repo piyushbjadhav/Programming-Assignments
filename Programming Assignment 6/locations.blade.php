1e4420fc7b641eee749d66182e53169d9432fb41
vagrant <vagrant@homestead>
2016-03-28 20:58:23 +0000

    files added

---
+++ Programming Assignment 6/locations.blade.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<b><i>Locations with stories:</i></b>
+		@foreach ($locations as $location)
+			<p>{{$location->value}}</p>
+            <ol>
+                @foreach ($location->stories as $story)
+                    <li>{{$story->title}}</li>
+                @endforeach
+            </ol>
+		@endforeach
+	</body>
+</html>
